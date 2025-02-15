2d7b6fbbd6e29b8ea7cd5160013bd247f8b8d5e9
Hycienth-Paul025 <166744375+Hycienth-Paul025@users.noreply.github.com>
2025-02-15 16:57:36 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,12 +7,10 @@ function updateClock() {
   let h = new Date().getHours();
   let m = new Date().getMinutes();
   let s = new Date().getSeconds();
-  let ampm = "AM";
+  let amPm = hours >= 12 ? "PM" : "AM";
 
-  if (hrs < 12) {
-    hrs = hrs - 12;
-    ampm = "PM";
-  }
+
+
 
   h = h < 10 ? "0" + h : h;
   m = m < 10 ? "0" + m : m;
