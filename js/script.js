d7fd6027287de5cd288df545ef614610335ea861
hycienth paul <hycienthp025@gmail.com>
2025-02-15 17:31:46 +0100

    great

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 const hrs = document.getElementById("hours");
 const mins = document.getElementById("minutes");
 const secs = document.getElementById("seconds");
-const ampm = document.getElementById("ampm");
+const ampm = document.getElementById("amPm");
 
 function updateClock() {
   let h = new Date().getHours();
   let m = new Date().getMinutes();
   let s = new Date().getSeconds();
-  let amPm = h >= 12 ? "PM" : "AM";
+  let meridian = h >= 12 ? "PM" : "AM"; 
 
   h = h < 10 ? "0" + h : h;
   m = m < 10 ? "0" + m : m;
@@ -16,7 +16,9 @@ function updateClock() {
   hrs.innerText = h;
   mins.innerText = m;
   secs.innerText = s;
+  ampm.innerText = meridian; // show "AMM & PM "
 }
+setInterval(updateClock, 1000);
 
 updateClock();
 
